562e21efd1a74de3b0d2f5cb5b374dc23df87d82
Christian Goetz <christian@lipalife.de>
2025-05-10 16:13:42 +0200

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.type === 'autofill') {
-    const [primary, headline, desc] = msg.text.split('\n').slice(0, 3);
-    document.querySelector('[data-testid="ad-primary-text"]').value = primary;
-    document.querySelector('[data-testid="ad-headline"]').value = headline;
-    document.querySelector('[data-testid="ad-description"]').value = desc;
-  }
-});
